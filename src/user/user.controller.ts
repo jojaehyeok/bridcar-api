7ccad75cfc56f1f6178ef19e73037a1dd24a760a
jojaehyeok <jjhst2285@naver.com>
2024-12-30 13:36:49 +0900

    attendence

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@Controller('user')
+@ApiTags('users') // Swagger에서 사용할 태그 지정
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
-  async createUser(
-    @Body() body: { name: string; studentNumber: string },
-  ): Promise<User> {
-    return this.userService.createUser(body.name, body.studentNumber);
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiResponse({ status: 201, description: 'User has been successfully created.', type: User })
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUserDto.name, createUserDto.studentNumber);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Retrieve all users' })
+  @ApiResponse({ status: 200, description: 'List of users', type: [User] })
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers();
   }
 }
