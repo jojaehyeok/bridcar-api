74316e574c88941598ef2fd7876cf3b137e73ddb
jojaehyeok <jjhst2285@naver.com>
2025-01-02 11:46:27 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-
-
 async function bootstrap() {
+  // 애플리케이션 생성
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors()
+  // CORS 활성화
+  app.enableCors();
+
   // Swagger 설정
   const config = new DocumentBuilder()
-    .setTitle('Attendance API')
-    .setDescription('API for managing attendance')
-    .setVersion('1.0')
-    .addTag('users')
+    .setTitle('Attendance API') // Swagger 제목
+    .setDescription('API for managing attendance') // 설명
+    .setVersion('1.0') // 버전
+    .addTag('users') // 태그 추가
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
+  // Swagger 문서 생성 및 경로 설정
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger UI: /api
+
+  // 글로벌 프리픽스 설정
+  app.setGlobalPrefix('api'); // 모든 경로 앞에 '/api'가 추가됨
+
+  // 애플리케이션 실행
   await app.listen(4000);
 }
 bootstrap();
