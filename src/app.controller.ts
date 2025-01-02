f69835082cd8a18cfae64c52ed0379da143a157f
jojaehyeok <jjhst2285@naver.com>
2025-01-02 11:31:28 +0900

    develop

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,12 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
+  @Get() // HTTP GET 요청 처리
+  getExample(): string {
+    return 'This is an example response!';
+  }
+  
 
 }
